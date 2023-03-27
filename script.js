1152df76dcc625584d63401d431b7ec51da555ae
Vinicius Fujarra <107464091+befujarra@users.noreply.github.com>
2023-03-27 00:19:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document
+  .getElementById("subLove")
+  .addEventListener("click", function (exibirImagem) {
+    document.getElementById("resultado").innerHTML =
+      "MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO MUITO vezes10000 &#128151; &#128151; &#128151; &#128151; &#128151; ";
+    var imagem = document.getElementById("imagemExibida");
+    imagem.src = "assets/pc.jpeg";
+  });
